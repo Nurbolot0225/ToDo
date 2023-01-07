adce305485ba47f60d228e8d6bccbdc5128b94ad
Nurbolot Berdibekov <nurchik0225@gmail.com>
2023-01-07 18:12:11 +0600

    fixed a bug

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ function getUserName(userId) {
     const user = users.find(u => u.id === userId)
     return user.name;
 }
-function printToDo({id, userId, title, completed}) {
+function printTodo({id, userId, title, completed}) {
     const li = document.createElement('li');
     li.className = 'todo-item';
     li.dataset.id = id;
@@ -22,35 +22,35 @@ function printToDo({id, userId, title, completed}) {
     status.checked = completed;
 
     const close = document.createElement('span')
-    status.innerHTML = '&times;';
-
+    close.className = 'close'
+    close.innerHTML = '&times;';
+ 
     li.prepend(status);
     li.append(close);
 
-    todoList.prepend('li');
+    todoList.prepend(li);
 }
 
 // Event Logic
 function initApp() {
     Promise.all([getAllTodos(), getAllUsers()]).then(values => {
         [todos, users] = values;
-        console.log(todos, users)
-    })
 
-    // Отправить в разметку
-    todos.forEach((todos => printToDo(todos)));
+        // Отправить в разметку
+        todos.forEach((todo => printTodo(todo)));
+    }) 
 }
 
 // Async Logic
 async function getAllTodos() {
-    const response = await fetch('https://jsonplaceholder.typicode.com/todos');
+    const response = await fetch("https://jsonplaceholder.typicode.com/todos");
     const data = await response.json();
 
     return data;
 }
 
 async function getAllUsers() {
-    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
     const data = await response.json();
 
     return data;
